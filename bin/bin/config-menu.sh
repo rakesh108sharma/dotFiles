00fd1df873f06eae4ff25d52175a9737a7dab2f1
rakesh108sharma <rakesh108sharma@gmail.com>
2019-12-15 14:16:33 +0100

    Firstly commit

---
+++ bin/bin/config-menu.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+choice=$(echo -e "csh\nbash\nfish\nxinitrc\nrc\nvim\nbspwm\nsxhkd\nTHIS-menu" | dmenu)
+
+case $choice in
+	csh)
+		xterm -e vim ~/.cshrc
+		;;
+	bash)
+		st -e vim ~/.bashrc
+		;;
+	fish)
+		st -e vim ~/.config/fish/config.fish
+			;;
+	xinitrc)
+		st -e vim ~/.xinitrc
+		;;
+	rc)
+		sudo st -e vim /etc/rc.conf
+		;;
+	vim)
+		st -e vim ~/.vimrc
+		;;
+	bspwm)
+		st -e vim ~/.config/bspwm/bspwmrc
+		;;
+	sxhkd)
+		st -e vim ~/.config/sxhkd/sxhkdrc
+		;;
+	THIS-menu)
+		st -e vim ~/bin/config-menu.sh
+esac
---
+++ vim/.vimrc
@@ -0,0 +1,5 @@
+nnoremap ,, :
+inoremap ,, <ESC>
+vnoremap ,, <ESC>
+
+
