2288109a942277b116f58dea9b37be779d6afa05
rakesh108sharma <rakesh108sharma@gmail.com>
2019-12-18 11:34:12 +0100

    fish + volumeicon added

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+set PATH ~/bin:$PATH
+alias yys 'apk search'
+alias yya 'sudo apk add'
+alias yyd 'sudo apk del'
+alias yyu 'sudo apk update && sudo apk upgrade'
+alias yyi 'apk info -a'
+alias yyl 'apk list -I | less' 
+alias qqq 'sudo poweroff'
+alias zzz 'sudo pm-suspend'
